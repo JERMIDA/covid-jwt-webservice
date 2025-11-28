24aad7ba0ed68845daf34a488acf854edc8ca883
JERMIDA <jeremydamena98@gmail.com>
2025-11-28 21:38:49 +0300

    Refactor server.js to use ES modules

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import dataRoutes from "./routes/data.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/auth", require("./routes/auth"));
-app.use("/data", require("./routes/data"));
+app.use("/auth", authRoutes);
+app.use("/data", dataRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Backend running on port ${process.env.PORT}`);
